b7b74a841dd8399c543ad2dbb8a9112ea8fededf
raziel214 <raziel214@users.noreply.github.com>
2018-09-11 16:32:37 -0300

    implemetation Sql Lite
    creation table in SQL Lite kotlin wiht  Anko

--- app/src/main/java/com/johnquimbaya/platzistore/DetailActivity.kt
+++ app/src/main/java/com/johnquimbaya/platzistore/DetailActivity.kt
@@ -3,6 +3,8 @@ package com.johnquimbaya.platzistore
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_description.*
+import kotlinx.android.synthetic.main.item_landing.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,9 +12,17 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
 
+        intent?.extras?.let {
+            txtDetailTitle.text=it.getString("title")
+            txtDetailDesc.text=it.getString("desc")
+            txtDetailPrice.text="$ ${String.format("%.2f", it.getDouble("price"))}"
+        }
+
        // intent?.let{
        //     val textFromMain:String = it.extras.getString("text")
         //    textDetail.text = textFromMain
        // }
+
+
     }
 }
